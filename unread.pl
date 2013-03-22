352e320770c26c8c5faf53d46e114ccaa6320b60
Lee Aylward <lee@laylward.com>
2013-03-21 21:11:59 -0500

    initial commit

---
+++ unread.pl
@@ -0,0 +1,31 @@
+use strict;
+use Irssi;
+use List::Util qw(max);
+use List::MoreUtils qw(any);
+
+my $VERSION = "0.1";
+
+my %IRSSI = (
+    authors     => "Lee Aylward",
+    contact     => "lee\@arstechnica.com",
+    name        => "unread",
+    description => "Adds a command to jump to next window with unread messages",
+    license     => "GPLv2",
+    url         => "http://github.com/leedo/irssi-unread",
+    changed     => "Thu Mar 21 19:41:00 2013",
+);
+
+Irssi::command_bind('unread', 'cmd_unread');
+
+sub cmd_unread {
+  my $win = Irssi::active_win()->{refnum};
+  my @wins = grep {any {$_->{data_level} > 1} $_->items} Irssi::windows;
+  my $max = max map {$_->{refnum}} @wins;
+
+  my @sorted = map {$_->[0]}
+              sort {$_->[1] <=> $_->[1]}
+               map {[$_, $_->{refnum} <= $win ? $_->{refnum} + $max : $_->{refnum}]}
+                   @wins;
+
+  $sorted[0]->set_active if @sorted;
+}
